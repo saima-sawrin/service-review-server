565ea0f8c2a666534dd193a3214855e485520bf6
Saima <saimasawrin324@gmail.com>
2022-11-08 21:29:34 +0600

    updated file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const jwt = require('jsonwebtoken');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,9 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9wy3smt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 // function verifyJWT(req, res, next){
 //     const authHeader = req.headers.authorization;
@@ -47,16 +49,16 @@ const serviceCollection = client.db('beautySalon').collection('Services');
 app.get('/services', async (req, res) => {
 const query = {}
 const cursor = serviceCollection.find(query);
-const services = await cursor.toArray();
+const services = await cursor.limit(3).toArray();
 res.send(services);
 });
 
-app.get('/services/:id', async (req, res) => {
-const id = req.params.id;
-const query = { _id: ObjectId(id) };
-const service = await serviceCollection.findOne(query);
-res.send(service);
-});
+// app.get('/services/:id', async (req, res) => {
+// const id = req.params.id;
+// const query = { _id: ObjectId(id) };
+// const service = await serviceCollection.findOne(query);
+// res.send(service);
+// });
 
 
 // orders api
