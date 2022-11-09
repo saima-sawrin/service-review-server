4fec1d9deffb14e3f4464c29dd828a3c5b6654c3
Saima <saimasawrin324@gmail.com>
2022-11-09 19:12:15 +0600

    all services index updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -52,13 +52,20 @@ const cursor = serviceCollection.find(query);
 const services = await cursor.limit(3).toArray();
 res.send(services);
 });
+app.get('/allServices', async (req, res) => {
+const query = {}
+const cursor = serviceCollection.find(query);
+const services = await cursor.toArray();
+res.send(services);
+});
 
-// app.get('/services/:id', async (req, res) => {
-// const id = req.params.id;
-// const query = { _id: ObjectId(id) };
-// const service = await serviceCollection.findOne(query);
-// res.send(service);
-// });
+app.get('/allServices/:id', async (req, res) => {
+const id = req.params.id;
+const query = { _id: ObjectId(id) };
+const service = await serviceCollection.findOne(query);
+console.log(service)
+res.send(service);
+});
 
 
 // orders api
